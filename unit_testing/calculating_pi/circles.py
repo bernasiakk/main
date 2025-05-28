1b8fcce65d432d8ac5062d810c824aeac1534705
Szymon Bernasiak <149424787+bernasiakk@users.noreply.github.com>
2025-05-28 08:22:04 +0200

    calc pi code

--- unit_testing/calculating_pi/circles.py
+++ unit_testing/calculating_pi/circles.py
@@ -2,5 +2,7 @@ from math import pi
 
 def circle_area(r):
     if r <= 0:
-        raise Exception("r must be a positive number")
+        raise ValueError("r must be a positive number")
+    elif not isinstance(r, (int, float)):
+        raise Exception("r must be a number")
     return pi*(r**2)
--- unit_testing/calculating_pi/test_circles.py
+++ unit_testing/calculating_pi/test_circles.py
@@ -8,13 +8,17 @@ class TestCircleArea(unittest.TestCase):
     def test_area(self):
         # Test area when radius >= 0
         self.assertAlmostEqual(circle_area(1), pi)
+        
         self.assertAlmostEqual(circle_area(2.1), pi * 2.1**2)
     
     def test_exceptions(self):
-        # self.assertRaises(Exception, circle_area, -1)
-        self.assertRaises(Exception, circle_area, 0)
-        with self.assertRaises(Exception):
+        self.assertRaises(ValueError, circle_area, 0)
+
+        with self.assertRaises(ValueError):
             circle_area(-1)
+        
+        with self.assertRaises(TypeError):
+            circle_area("test")
 
 
 if __name__ == '__main__':
