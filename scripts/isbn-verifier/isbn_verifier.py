927cc599f90e740904d7877699e8fb17b33103de
Szymon Bernasiak <149424787+bernasiakk@users.noreply.github.com>
2025-07-06 17:03:47 +0200

    add list comprehension and lint the code

--- scripts/isbn-verifier/isbn_verifier.py
+++ scripts/isbn-verifier/isbn_verifier.py
@@ -1,35 +1,33 @@
+
+"""Module for validating ISBN-10 numbers."""
 import re
 
 def is_valid(isbn):
+
+    """Check if the provided ISBN-10 is valid."""
     isbn = str(isbn)
-    
-    # TODO change this so it checks if X is only at the last place??
+
+    # TODO Check if X is only at the last place
     if not re.fullmatch(r"[0-9X\-]+", isbn):
         return False
-    
-    isbn = re.sub('[^0-9X]','', isbn)
-    
-    
-    if not len(isbn) == 10:
+
+    isbn = re.sub(r'[^0-9X]', '', isbn)
+
+    if len(isbn) != 10:
         return False
 
     isbn = list(isbn)
 
     if 'X' in isbn:
-        if not isbn[-1] == 'X':
+        if isbn[-1] != 'X':
             return False
         isbn[-1] = '10'
-    
-    # TODO list comprehension
-    isbn_sum = 0
-    
-    for number, weight in zip(isbn, range(10,0,-1)):
-        isbn_sum += int(number) * weight
-    
-    if isbn_sum % 11 == 0:
-        return True
-    return False
-        
+
+    # Use list comprehension for sum
+    isbn_sum = sum(int(number) * weight for number, weight in zip(isbn, range(10, 0, -1)))
+
+    return isbn_sum % 11 == 0
+
 
 if __name__ == "__main__":
     print(is_valid("3-598-21508-8"))
