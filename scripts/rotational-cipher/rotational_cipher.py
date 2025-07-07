ce5b8931b61486d7aa4da015917fac19e2062f2b
Szymon Bernasiak <149424787+bernasiakk@users.noreply.github.com>
2025-07-07 07:50:25 +0200

    .

--- scripts/rotational-cipher/rotational_cipher.py
+++ scripts/rotational-cipher/rotational_cipher.py
@@ -1,8 +1,5 @@
 def rotate(text, key):
     """
-    TODO TODO I think it's pretty much it... Just fix the TODO's below, then run the tests, pylint and that's it.
-    ---
-    - TODO handle the spaces, numbers & special chars (you'll have to remove, then return them in the right places)
     """
     if not isinstance(key, int):
         raise TypeError("Key must be an integer.")
@@ -10,18 +7,20 @@ def rotate(text, key):
     if key > 27:
         raise ValueError("Key must be of length less than 28.")
     
-    plain_list = list("abcdefghijklmnopqrstuvwxyz")
-    # TODO I think you can remove this one, actually
-    cipher_list = [plain_list[:] for _ in range(key)]
-        
-    # TODO change name
+    plain_list = list("abcdefghijklmnopqrstuvwxyz")*2
+
     result = []
     
-    for letter in text:
-        result.append(plain_list[plain_list.index(letter) + key])
+    # TODO list comprehension??
+    # TODO TODO test if this one handles numbers and special chars. Then think how to handle UPPERCASE letters
+    for item in text:
+        if item in plain_list:
+            result.append(plain_list[plain_list.index(item) + key])
+        else:
+            result.append(item)
     
     return ''.join(result)
     
     
 if __name__ == "__main__":
-    print(rotate('omg', 5))
+    print(rotate('omy', 5))
