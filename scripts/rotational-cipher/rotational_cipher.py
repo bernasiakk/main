1d012b7f48df16506786d848e389decc0692b72d
Szymon Bernasiak <149424787+bernasiakk@users.noreply.github.com>
2025-07-08 06:53:26 +0200

    add uppercase handling & lint the script

--- scripts/rotational-cipher/rotational_cipher.py
+++ scripts/rotational-cipher/rotational_cipher.py
@@ -1,26 +1,23 @@
 def rotate(text, key):
-    """
-    """
+    """Rotate the given text by the specified key using a rotational cipher."""
     if not isinstance(key, int):
         raise TypeError("Key must be an integer.")
-    
     if key > 27:
-        raise ValueError("Key must be of length less than 28.")
-    
-    plain_list = list("abcdefghijklmnopqrstuvwxyz")*2
+        raise ValueError("Key must be less than 28.")
 
+    plain_list = list("abcdefghijklmnopqrstuvwxyz") * 2
     result = []
-    
-    # TODO list comprehension??
-    # TODO TODO test if this one handles numbers and special chars. Then think how to handle UPPERCASE letters
+
     for item in text:
         if item in plain_list:
             result.append(plain_list[plain_list.index(item) + key])
+        elif item.lower() in plain_list:
+            result.append(plain_list[plain_list.index(item.lower()) + key].upper())
         else:
             result.append(item)
-    
+
     return ''.join(result)
-    
-    
+
+
 if __name__ == "__main__":
-    print(rotate('omy', 5))
+    print(rotate('Omy-omy', 5))
